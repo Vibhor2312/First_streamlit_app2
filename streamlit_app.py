cb38bc370ea7429feacee09a580537e5f1ed6bf7
Vibhor2312 <145025847+Vibhor2312@users.noreply.github.com>
2023-09-15 13:40:05 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,3 +1,7 @@
 import streamlit
 streamlit.title('my parets new healthy diner')
 
+streamlit.header('Breakfast menu')
+streamlit.text('omega 3 & blueberry oatmeal')
+streamlit.text('kale, spinach & Rocket smoothie')
+streamlit.text('hard- Boiled Free-Range Egg')
