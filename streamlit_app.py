1fb5918e28e93b48c097726a9a7aa7c5a4c31d7e
Vibhor2312 <145025847+Vibhor2312@users.noreply.github.com>
2023-09-22 16:54:56 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,1 +1,15 @@
 
+
+import streamlit
+import pandas
+import requests
+import snowflake.connector
+from urllib.error import URLError
+
+streamlit.title('my parets new healthy diner')
+
+streamlit.header('Breakfast menu')
+streamlit.text('🥣 Omega 3 & blueberry oatmeal')
+streamlit.text(' 🥗 kale, spinach & Rocket smoothie')
+streamlit.text(' 🐔 hard- Boiled Free-Range Egg')
+streamlit.text('🥑🍞Avacado Toast')
