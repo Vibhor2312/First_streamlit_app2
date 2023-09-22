076d33a5a950b910649c4d8bae7c7d184d0a151f
Vibhor2312 <145025847+Vibhor2312@users.noreply.github.com>
2023-09-22 18:20:50 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -42,8 +42,8 @@ Add_my_fruit=  streamlit.text_input('What fruit would you like to add','jackfrui
 streamlit.write('Thanks for adding jackfruit ', fruit_choice)
 my_cur.execute("insert into fruit_load_list values ('from streamlit')")
 
-except URLError as e:
-    streamlit.error()
+  except URLError as e:
+     streamlit.error()
 
 
 def get_fruityvice_data(this_fruit_choice):
