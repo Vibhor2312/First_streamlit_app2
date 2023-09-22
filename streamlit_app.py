7f8da14fbc0178e159576c3da088db46b07fc507
Vibhor2312 <145025847+Vibhor2312@users.noreply.github.com>
2023-09-22 17:26:30 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -45,9 +45,9 @@ streamlit.write('The user entered ', fruit_choice)
 my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
 my_cur = my_cnx.cursor()
 my_cur.execute("SELECT * from fruit_load_list")
-my_data_row = my_cur.fetchone()
-streamlit.text("fruit load contains:")
-streamlit.text(my_data_row)
+my_data_rows = my_cur.fetchall()
+streamlit.header("fruit load contains:")
+streamlit.dataframe(my_data_rows)
 
 
 
